41918653c363d7db65330465eacd651eccfbc96c
albertnievesmoreno <albertnievesmoreno@S1-10>
2017-02-16 11:32:08 +0100

    Añadido el operator

--- RandomEnemyGenerator/RandomEnemyGenerator.cpp
+++ RandomEnemyGenerator/RandomEnemyGenerator.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 enum EnemyType { zombie, vampire, ghost, witch };
-struct { EnemyType type; string name; int health; };
+struct Enemy { EnemyType type; string name; int health; };
+
+bool operator == (Enemy A, Enemy B) 
+{
+	if (A.type == B.type && A.name == B.name) {
+		
+	}
+}
